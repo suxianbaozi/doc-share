e3c9a054f1d231e15754389875e4d92dc4b1d9bb
王秋石 <qiushi@reco.cn>
2013-08-15 18:15:00 +0800

    he

--- spider.py
+++ spider.py
@@ -17,26 +17,46 @@ ISOTIMEFORMAT='%Y-%m-%d %X'
 total = 0
 totalGot = 0
 
+
 getViewThreads = {}
 
+listThreadNum = 50
+viewThreadNum = 50
 
 pIndex = 0
-proxies = ['173.213.113.111:8089',]
-#proxies = ['',] '110.4.12.170:80','173.213.113.111:8089'
+#proxies = ['110.4.12.170:80',]
+proxies = [
+           '221.10.40.232:81',
+           '121.26.229.197:8081',
+           '60.8.63.52:8081',
+           '125.91.4.146:3328',
+           '221.10.40.236:83',
+           '202.116.160.89:80',
+           '222.180.173.10:8080',
+           '203.86.79.227:80',
+           '173.213.113.111:8089',
+           ''
+           ] #'110.4.12.170:80','173.213.113.111:8089'
 
 
-
-def getUrlContent(url):
+def changeProxy():
     global pIndex
     proxy = proxies[pIndex]
     pIndex += 1
+    
     if pIndex == len(proxies):
         pIndex = 0
+        
     if proxy:
         proxy_support = urllib2.ProxyHandler({'http':'http://'+proxy})  
         opener = urllib2.build_opener(proxy_support, urllib2.HTTPHandler)
         urllib2.install_opener(opener)
-        
+    else:
+        opener = urllib2.build_opener(urllib2.BaseHandler, urllib2.HTTPHandler)
+        urllib2.install_opener(opener)
+
+
+def getUrlContent(url):
     req = urllib2.Request(url)
     req.add_header("Referer","http://www.chemicalbook.com/")
     req.add_header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36")
@@ -50,35 +70,36 @@ def forSafe():
     global totalGot
     global total
     
-    if total%900==0 and total>0:
-        print '900 ','sleep 10 minutes'
+    if total%500==0 and total>0:
+        print 'oh  500 ,change proxy'
+        changeProxy()
+        
         
         for k,thread in getViewThreads.items():
-            thread.wait = 60*10 #休息十分钟
+            thread.waitLong = 60*10*0.5 #休息十分钟
             try:
                 thread.fp.close()
             except:
                 pass
-        
-        try:
-            getUrlContent('http://www.chemicalbook.com/')
-            time.sleep(1)
-            getUrlContent('http://www.chemicalbook.com/ProductCASList_13_0_EN.htm');
-            time.sleep(1)
-            getUrlContent('http://www.chemicalbook.com/BuyingLeadList_EN.aspx');
-            time.sleep(1)
-        except:
-            try:
-                getUrlContent('http://www.chemicalbook.com/')
-                time.sleep(1)
-                getUrlContent('http://www.chemicalbook.com/ProductCASList_13_0_EN.htm');
-                time.sleep(1)
-                getUrlContent('http://www.chemicalbook.com/BuyingLeadList_EN.aspx');
-                time.sleep(1)
-            except:
-                pass
-            return
-        return
+        #try:
+        #    getUrlContent('http://www.chemicalbook.com/')
+        #    time.sleep(1)
+        #    getUrlContent('http://www.chemicalbook.com/ProductCASList_13_0_EN.htm');
+        #    time.sleep(1)
+        #    getUrlContent('http://www.chemicalbook.com/BuyingLeadList_EN.aspx');
+        #    time.sleep(1)
+#        except:
+#            try:
+#                getUrlContent('http://www.chemicalbook.com/')
+#                time.sleep(1)
+#                getUrlContent('http://www.chemicalbook.com/ProductCASList_13_0_EN.htm');
+#                time.sleep(1)
+#                getUrlContent('http://www.chemicalbook.com/BuyingLeadList_EN.aspx');
+#                time.sleep(1)
+#            except:
+#                pass
+#            return
+#        return
     
     if total%100==0 and total>0:
         for k,thread in getViewThreads.items():
@@ -115,15 +136,20 @@ class getUrl(Thread):
         Thread.__init__(self)
         self.tid = tid
         self.wait = 0
-        
+        self.waitLong = 0
     def run(self):
         global total
         while True:
             if self.wait>0:
-                #print 'thread list_',self.tid,' waiting...'
+                print 'thread list_',self.tid,' waiting...'
                 time.sleep(self.wait)
                 self.wait = 0
-            
+            if self.waitLong>0:
+                print 'thread list_',self.tid,' waitingLong...'
+                time.sleep(self.waitLong)
+                self.waitLong = 0
+            if listUrlList.qsize()==0:
+                break
             id = listUrlList.get()
             url =  baseUrl%(id,)
             #print url
@@ -156,6 +182,7 @@ class getView(Thread):
         Thread.__init__(self)
         self.tid = tid
         self.wait = 0
+        self.waitLong = 0
     def run(self):
         global total
         global totalGot
@@ -164,7 +191,12 @@ class getView(Thread):
                 print 'thread view_',self.tid,' waiting...'
                 time.sleep(self.wait)
                 self.wait = 0
-            
+                
+            if self.waitLong>0:
+                print 'thread view_',self.tid,' waitingLong...'
+                time.sleep(self.waitLong)
+                self.waitLong = 0
+                
             id = viewUrlList.get()
             viewUrl = viewBaseUrl%(id,)
             #print url
@@ -213,16 +245,42 @@ class Timer(Thread):
                   
 print time.strftime( ISOTIMEFORMAT, time.localtime() )
 
+#检查代理可用
+
+for i in range(0,range(len(proxies))):
+    
+    changeProxy()
+    
+    cp = pIndex-1;
+    
+    print 'check ',proxies[cp],'...'
+    
+    try:
+        c  = getUrlContent('http://www.baidu.com');
+        if 'baidu' not in c:
+            print proxies[cp],' seemed not available remove it';
+            proxies.remove(proxies[cp])
+        else:
+            print proxies[cp],' available'
+    except:
+        cp = pIndex-1;
+        print proxies[cp],' seemed not available remove it';
+        proxies.remove(proxies[cp])
+    
+
+
+pIndex = 0
+changeProxy()
 
 for i in range(0,101):
     listUrlList.put(i)
 
-for i in range(0,50):
+for i in range(0,listThreadNum):
     getViewThreads['list_'+str(i)] = getUrl(i)
     getViewThreads['list_'+str(i)].start()
 
 #get view start...
-for i in range(0,50):
+for i in range(0,viewThreadNum):
     getViewThreads['view_'+str(i)] = getView(i)
     getViewThreads['view_'+str(i)].start()
 
